4e807b08bf4c531955305ac62e99df784d84c0ba
mabrodi <mabrodi7@gmail.com>
2025-02-15 22:06:35 +0200

    add annotation Display name to test

--- src/test/java/my/project/orm/DefaultQueryGeneratorTest.java
+++ src/test/java/my/project/orm/DefaultQueryGeneratorTest.java
@@ -2,6 +2,7 @@ package my.project.orm;
 
 import my.project.orm.annotation.Table;
 import my.project.orm.entity.Person;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,24 +12,28 @@ public class DefaultQueryGeneratorTest {
 
     private final DefaultQueryGenerator queryGenerator = new DefaultQueryGenerator();
 
+    @DisplayName("test find all generates correct select query")
     @Test
     public void testFindAllGeneratesCorrectSelectQuery() {
         String query = queryGenerator.findAll(Person.class);
         assertEquals("SELECT id, name, salary FROM Person;", query);
     }
 
+    @DisplayName("test find by id generates correct select query with id condition")
     @Test
     public void testFindByIdGeneratesCorrectSelectQueryWithIdCondition() {
         String query = queryGenerator.findById(Person.class, 1);
         assertEquals("SELECT id, name, salary FROM Person where id = 1;", query);
     }
 
+    @DisplayName("test delete by id generates correct delete query with id condition")
     @Test
     public void testDeleteByIdGeneratesCorrectDeleteQueryWithIdCondition() {
         String query = queryGenerator.deleteById(Person.class, 1);
         assertEquals("DELETE FROM Person where id = 1;", query);
     }
 
+    @DisplayName("test insert generates correct insert query with values")
     @Test
     public void testInsertGeneratesCorrectInsertQueryWithValues() throws IllegalAccessException {
         Person person = new Person(1, "Dima", 200.4);
@@ -36,6 +41,7 @@ public class DefaultQueryGeneratorTest {
         assertEquals("INSERT INTO Person (id, name, salary) VALUES ('1', 'Dima', '200.4');", query);
     }
 
+    @DisplayName("test update generates correct update query with values and id condition")
     @Test
     public void testUpdateGeneratesCorrectUpdateQueryWithValuesAndIdCondition() throws IllegalAccessException {
         Person person = new Person(1, "Dima", 200.4);
@@ -43,12 +49,14 @@ public class DefaultQueryGeneratorTest {
         assertEquals("UPDATE Person SET id = '1', name = 'Dima', salary = '200.4' WHERE id = 1;", query);
     }
 
+    @DisplayName("test find all throws exception when class is not annotated with table")
     @Test
     public void testFindAllThrowsExceptionWhenClassIsNotAnnotatedWithTable() {
         class NotAnnotatedClass {}
         assertThrows(IllegalArgumentException.class, () -> queryGenerator.findAll(NotAnnotatedClass.class));
     }
 
+    @DisplayName("test find all throws exception when class has no columns annotated with column")
     @Test
     public void testFindAllThrowsExceptionWhenClassHasNoColumnsAnnotatedWithColumn() {
         @Table(name = "EmptyClass")
